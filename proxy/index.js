9fe23d42ce2924f948e53873d650c51a94c24fba
j-tirelli <john.tirelli@gmail.com>
2020-10-11 23:45:30 -0700

    First attept at adding hero docker iamge

--- proxy/index.js
+++ proxy/index.js
@@ -1,23 +1,33 @@
 const express = require('express');
-const morgan = require("morgan");
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const port = 3000;
 
 const app = express();
+app.locals.getGameId = (referer) => {
+  if (referer[referer.length - 1] === '/') {
+    referer = referer.slice(0, -1)
+  }
+  var refererPieces = referer.split('/');
+  return refererPieces[refererPieces.length - 1]
+};
+
 app.use(bodyParser.json())
 
-  app.use(express.static('public'));
-  app.use('/game/:id/', (req, res, next) => {
-    var id = req.params.id;
-    app.locals.gameID = id;
-    next();
-  }, express.static('public'));
+app.use(express.static('public'));
+app.use('/game/:id/', (req, res, next) => {
+  debugger;
+  var id = req.params.id;
+  next();
+}, express.static('public'));
 
   // //////////////Hero Section////////////////// //
   app.use('/api/hero/all_info/', (req, res) => {
-    axios.get(`http://127.0.0.1:3001/api/hero/all_info/${app.locals.gameID}`)
+    let host = req.headers.host;
+    let ip = host.split(':')[0];
+    var gameId = app.locals.getGameId(req.headers.referer);
+    axios.get(`http://${ip}:3001/api/hero/all_info/${gameId}`)
     .then(function (response) {
       res.send(response.data);
     })
@@ -29,9 +39,11 @@ app.use(bodyParser.json())
 
   // //////////////Body Section////////////////// //
   app.get('/moist-air/game', (req, res) => {
-    axios.get(`http://127.0.0.1:3002/moist-air/game?id=${app.locals.gameID}`)
+    debugger;
+    var gameId = app.locals.getGameId(req.headers.referer);
+    axios.get(`http://127.0.0.1:3002/moist-air/game`)
     .then(function (response) {
-      res.send([ response.data[app.locals.gameID - 1] ]);
+      res.send([ response.data[gameId - 1] ]);
     })
     .catch(function (error) {
       console.log(error);
@@ -41,18 +53,29 @@ app.use(bodyParser.json())
 
   // //////////////Review Section////////////////// //
   app.get('/moist-air/reviews', (req, res) => {
-    axios.get(`http://127.0.0.1:3003/moist-air/reviews?gameID=${app.locals.gameID}`)
-    .then(function (response) {
-      res.send(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-      res.send(error);
-    });
+    let host = req.headers.host;
+    let ip = host.split(':')[0];
+    // debugger;
+    // let reviewsComponent = "reviews";
+    // console.log('---------------------------------')
+    // console.log('req.headers\n', req.headers)
+    // console.log('---------------------------------')
+    if (req.headers.referer) {
+      var gameId = app.locals.getGameId(req.headers.referer);
+      axios.get(`http://${ip}:3003/moist-air/reviews?gameID=${gameId}`)
+      .then(function (response) {
+        res.send(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        // Control the error sent back.
+        res.send(error);
+      });
+    }
   })
 
   app.patch('/moist-air/reviews', (req, res) => {
-    // debugger;
+    debugger;
     let reviewID = req.query.reviewID;
     let key = req.query.key;
     let value = req.query.value;
